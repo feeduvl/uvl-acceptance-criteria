9b99ebddad30d6f504ad2a1feb3de63f983dd443
Marvin A. Ruder <ruder.marvin@icloud.com>
2021-11-18 16:58:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# uvl-acceptance-criteria  [![badge](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.de.html)
+
+## About
+
+This is a microservice that can run in a docker container and generates acceptance criteria from user stories when queried by a REST call.
+
+## REST API
+
+See [swagger.yaml](../master/swagger.yaml) for details. The tool at https://editor.swagger.io/ can be used to render the swagger file.
+
+## Method Parameter
+
+None.
+
+## License
+Free use of this software is granted under the terms of the [GPL version 3](https://www.gnu.org/licenses/gpl-3.0.de.html) (GPL 3.0).
---
+++ src/main/java/de/uhd/ifi/se/acgen/App.java
@@ -0,0 +1,37 @@
+package de.uhd.ifi.se.acgen;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import static spark.Spark.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        port(9696);
+        
+        get("/hitec/generate/acceptance-criteria/status", (request, response) -> {
+            System.out.println("Status triggered.");
+            response.header("Content-Type", "application/json");
+            JsonObject jsonResponse = new JsonObject();
+            jsonResponse.addProperty("status", "in development");
+            return jsonResponse;
+        });
+        post("/hitec/generate/acceptance-criteria/run", (request, response) -> {
+            System.out.println("Run triggered.");
+            try {
+                JsonObject jsonRequest = new Gson().fromJson(request.body(), JsonObject.class);
+                response.header("Content-Type", "application/json");
+                JsonObject documents = jsonRequest.get("dataset").getAsJsonObject().get("documents").getAsJsonObject();
+                JsonArray jsonResponse = new JsonArray();
+                jsonResponse.add("The request contains " + documents.size() + " documents.");
+                return jsonResponse;     
+            } catch (Exception e) {
+                response.status(500);
+                return "<h1>500 Internal Server Error</h1>";
+            }
+        });
+    }
+}
