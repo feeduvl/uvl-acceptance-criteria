38cdcbe687be4ec017e6933b6c15856048d00fbf
Marvin A. Ruder <ruder.marvin@icloud.com>
2021-12-07 17:40:10 +0100

    Add tests for acceptance criterion model

--- src/test/java/de/uhd/ifi/se/acgen/model/TestAcceptanceCriterion.java
+++ src/test/java/de/uhd/ifi/se/acgen/model/TestAcceptanceCriterion.java
@@ -1,6 +1,8 @@
 package de.uhd.ifi.se.acgen.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,7 @@ public class TestAcceptanceCriterion {
     public void testRoleAcceptanceCriterion() {
         AcceptanceCriterion roleAcceptanceCriterion = new AcceptanceCriterion("a developer", AcceptanceCriterionType.ROLE);
         assertEquals(AcceptanceCriterionType.ROLE, roleAcceptanceCriterion.getType());
+        assertFalse(roleAcceptanceCriterion.getType().isLog());
         assert(roleAcceptanceCriterion.getRawString().equals("a developer"));
         assert(roleAcceptanceCriterion.toString().equals("GIVEN a developer is using the software"));
     }
@@ -18,6 +21,7 @@ public class TestAcceptanceCriterion {
     public void testUIAcceptanceCriterion() {
         AcceptanceCriterion uiAcceptanceCriterion = new AcceptanceCriterion("the example view under \"more examples\"", AcceptanceCriterionType.UI);
         assertEquals(AcceptanceCriterionType.UI, uiAcceptanceCriterion.getType());
+        assertFalse(uiAcceptanceCriterion.getType().isLog());
         assert(uiAcceptanceCriterion.getRawString().equals("the example view under \"more examples\""));
         assert(uiAcceptanceCriterion.toString().equals("GIVEN the active user interface is the example view under \"more examples\""));
     }
@@ -26,18 +30,47 @@ public class TestAcceptanceCriterion {
     public void testConditionalAcceptanceCriterion() {
         AcceptanceCriterion causeAcceptanceCriterion = new AcceptanceCriterion("he, she, it", AcceptanceCriterionType.CAUSE);
         assertEquals(AcceptanceCriterionType.CAUSE, causeAcceptanceCriterion.getType());
+        assertFalse(causeAcceptanceCriterion.getType().isLog());
         assert(causeAcceptanceCriterion.getRawString().equals("he, she, it"));
         assert(causeAcceptanceCriterion.toString().equals("WHEN he, she, it"));
 
         AcceptanceCriterion effectAcceptanceCriterion = new AcceptanceCriterion("das s muss mit", AcceptanceCriterionType.EFFECT);
         assertEquals(AcceptanceCriterionType.EFFECT, effectAcceptanceCriterion.getType());
+        assertFalse(effectAcceptanceCriterion.getType().isLog());
         assert(effectAcceptanceCriterion.getRawString().equals("das s muss mit"));
         assert(effectAcceptanceCriterion.toString().equals("THEN das s muss mit"));
 
         AcceptanceCriterion causeInteractionAcceptanceCriterion = new AcceptanceCriterion("on a sample button", AcceptanceCriterionType.CAUSE_INTERACTION);
         assertEquals(AcceptanceCriterionType.CAUSE_INTERACTION, causeInteractionAcceptanceCriterion.getType());
+        assertFalse(causeInteractionAcceptanceCriterion.getType().isLog());
         assert(causeInteractionAcceptanceCriterion.getRawString().equals("on a sample button"));
         assert(causeInteractionAcceptanceCriterion.toString().equals("WHEN the user clicks on a sample button"));
     }
 
+    @Test
+    public void testLogAcceptanceCriterion() {
+        AcceptanceCriterion errorAcceptanceCriterion = new AcceptanceCriterion("This is a severe error!", AcceptanceCriterionType.ERROR);
+        assertEquals(AcceptanceCriterionType.ERROR, errorAcceptanceCriterion.getType());
+        assertTrue(errorAcceptanceCriterion.getType().isLog());
+        assert(errorAcceptanceCriterion.getRawString().equals("This is a severe error!"));
+        assert(errorAcceptanceCriterion.toString().equals("ERROR: This is a severe error!"));
+
+        AcceptanceCriterion warningAcceptanceCriterion = new AcceptanceCriterion("This is a less severe warning.", AcceptanceCriterionType.WARNING);
+        assertEquals(AcceptanceCriterionType.WARNING, warningAcceptanceCriterion.getType());
+        assertTrue(warningAcceptanceCriterion.getType().isLog());
+        assert(warningAcceptanceCriterion.getRawString().equals("This is a less severe warning."));
+        assert(warningAcceptanceCriterion.toString().equals("WARNING: This is a less severe warning."));
+
+        AcceptanceCriterion infoAcceptanceCriterion = new AcceptanceCriterion("This is helpful information.", AcceptanceCriterionType.INFO);
+        assertEquals(AcceptanceCriterionType.INFO, infoAcceptanceCriterion.getType());
+        assertTrue(infoAcceptanceCriterion.getType().isLog());
+        assert(infoAcceptanceCriterion.getRawString().equals("This is helpful information."));
+        assert(infoAcceptanceCriterion.toString().equals("INFO: This is helpful information."));
+
+        AcceptanceCriterion debugAcceptanceCriterion = new AcceptanceCriterion("Beep boop!", AcceptanceCriterionType.DEBUG);
+        assertEquals(AcceptanceCriterionType.DEBUG, debugAcceptanceCriterion.getType());
+        assertTrue(debugAcceptanceCriterion.getType().isLog());
+        assert(debugAcceptanceCriterion.getRawString().equals("Beep boop!"));
+        assert(debugAcceptanceCriterion.toString().equals("DEBUG: Beep boop!"));
+    }
 }
